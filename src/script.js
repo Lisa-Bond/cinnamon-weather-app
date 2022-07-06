a6458dcd63c65bc9ff9366c9d8c1f74fdc3e3510
Lisa Bondarenko <lisa.bond.github@gmail.com>
2022-07-06 21:47:41 +0300

    Added getForecast function

--- src/script.js
+++ src/script.js
@@ -126,3 +126,9 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6b45fead1f572a2847620f61855bb862";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
